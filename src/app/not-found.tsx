bbf6a3a9805a2151a18c67560d5e68b2aa44ef42
Jay Holly <jaymacbook@Jays-MBP.lan>
2026-08-04 00:13:16 -0400

    Fix linter errors in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "3rem" }}>
       <h1>404 - Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
+      <p>The page you&rsquo;re looking for doesn&rsquo;t exist.</p>
     </div>
   );
 }
